7c4b33c65ba0f9fa81df93e640b3806c04ab26e5
Eighthsum9798 <1633236@fife.ac.uk>
2023-02-27 15:57:46 +0000

    added for loops and worked with arrays

--- Alien-arch-unity/Assets/Scripts/TestScript.cs
+++ Alien-arch-unity/Assets/Scripts/TestScript.cs
@@ -6,14 +6,62 @@ public class TestScript : MonoBehaviour
 {
 
 
+
+    public string[] messages;
+
     public string startMessage;
 
     public string updateMessage;
 
+    public string message1, message2, message3;
+
+    public int messageChoice;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(startMessage);
+
+
+        
+
+        if (messageChoice < messages.Length && messageChoice >=0)
+        {
+            Debug.Log(messages[messageChoice]);
+
+        }
+
+        else
+        {
+            Debug.Log("invalid choice");
+        }
+        
+        
+        
+        //Debug.Log(startMessage);
+
+        //MessageChoice = Random.Range(1, 4);
+
+        /*
+        if (MessageChoice == 1)
+        {
+
+            Debug.Log(messages[0]);
+
+        }
+        else if (MessageChoice == 2)
+        {
+            Debug.Log(messages[1]);
+        }
+        else if (MessageChoice == 3)
+        {
+            Debug.Log(messages[2]);
+        }
+        else
+        {
+            Debug.Log("invalid number");
+        }
+
+        */
     }
 
     // Update is called once per frame
