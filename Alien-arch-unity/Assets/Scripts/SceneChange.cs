42b726b85927809341ca070ce24c3304f61115aa
Eighthsum9798 <1633236@fife.ac.uk>
2023-03-20 15:55:28 +0000

    added movement and menu functionality

--- Alien-arch-unity/Assets/Scripts/SceneChange.cs
+++ Alien-arch-unity/Assets/Scripts/SceneChange.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class SceneChange : MonoBehaviour
     public void ChangeToScene(string targetSceneName)
     {
 
-
+        SceneManager.LoadScene(targetSceneName);
 
 
     }
